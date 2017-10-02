98bf1a116ff3769e5618b1af7fda823a4934ef5e
juan987 desde casa <olonimu@gmail.com>
2017-10-02 20:24:22 +0200

    Version que le envie a cesar

--- app/src/main/java/com/juan/mezclar/Mezclar.java
+++ app/src/main/java/com/juan/mezclar/Mezclar.java
@@ -7,8 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Mezclar extends AppCompatActivity {
+//Para gestionar imagenes
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+public class Mezclar extends AppCompatActivity {
+    private ImageView collageImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +36,32 @@ public class Mezclar extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        collageImage = (ImageView)findViewById(R.id.imageView3);
+
+        Button combineImage = (Button)findViewById(R.id.combineimage);
+        combineImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bigImage = BitmapFactory.decodeResource(getResources(), R.drawable.avion1);
+                Bitmap smallImage = BitmapFactory.decodeResource(getResources(), R.drawable.transparente3);
+                Bitmap mergedImages = createSingleImageFromMultipleImages(bigImage, smallImage);
+
+                collageImage.setImageBitmap(mergedImages);
+            }
+        });
     }
+
+
+    private Bitmap createSingleImageFromMultipleImages(Bitmap firstImage, Bitmap secondImage){
+
+        Bitmap result = Bitmap.createBitmap(firstImage.getWidth(), firstImage.getHeight(), firstImage.getConfig());
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(firstImage, 0f, 0f, null);
+        canvas.drawBitmap(secondImage, 10, 10, null);
+        return result;
+    }
+
+    //hjhjjhkhkhkhkhk
 
 }
